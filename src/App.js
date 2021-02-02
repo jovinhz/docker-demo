17b40914dba4f443dde2dbd0cab7d04b6e872c83
jovinhz <jovinharris@gmail.com>
2021-02-02 13:47:18 +0700

    change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is new feature!
         </a>
       </header>
     </div>
